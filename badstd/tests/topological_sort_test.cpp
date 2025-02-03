a8408cf239b149b22a1bb7307d1913c6db4e054a
baranyildirim@gmail.com <baran.yildirim@outlook.com>
2025-02-03 07:00:07 +0000

    topo sort basic

--- badstd/tests/topological_sort_test.cpp
+++ badstd/tests/topological_sort_test.cpp
@@ -11,3 +11,10 @@ TEST(TopologicalSortTest, RegularGraph) {
   auto solution = sort(edges);
   EXPECT_THAT(solution, testing::Eq(std::vector<int>{0, 1, 2}));
 }
+
+TEST(TopologicalSortTest, RegularGraphAll) {
+  auto edges = EdgeList{Edge{0, 2}, Edge{1, 2}, Edge{2, 3}};
+  auto solution = sort_all(edges);
+  EXPECT_THAT(solution, testing::Eq(std::vector<std::vector<int>>{
+                            {1, 0, 2, 3}, {0, 1, 2, 3}}));
+}
--- badstd/topological_sort.h
+++ badstd/topological_sort.h
@@ -41,3 +41,56 @@ inline std::vector<int> sort(const EdgeList& graph) {
              ? solution
              : std::vector<int>{};
 }
+
+inline std::vector<std::vector<int>> sort_all(const EdgeList& graph) {
+  auto node_deps = std::unordered_map<int, std::unordered_set<int>>{};
+  auto node_edges = std::unordered_map<int, std::unordered_set<int>>{};
+  for (const auto& [from, to] : graph) {
+    node_deps[to].insert(from);
+    node_deps[from];
+    node_edges[from].insert(to);
+    node_edges[to];
+  }
+  auto start_nodes = std::queue<int>{};
+  for (const auto& [node, deps] : node_deps) {
+    if (deps.empty()) start_nodes.push(node);
+  }
+  auto all_sorts = std::vector<std::vector<int>>{};
+  const auto sort_all_recursive = [&all_sorts](const auto& self, auto solution,
+                                               auto start_nodes, auto node_deps,
+                                               auto node_edges) -> void {
+    auto seen_nodes = std::unordered_set<int>{};
+    while (!start_nodes.empty()) {
+      auto curr_node = start_nodes.front();
+      if (seen_nodes.contains(curr_node)) break;
+      seen_nodes.insert(curr_node);
+      start_nodes.pop();
+      auto curr_start_nodes = start_nodes;
+      start_nodes.push(curr_node);
+
+      auto curr_solution = solution;
+      curr_solution.push_back(curr_node);
+
+      auto curr_node_deps = node_deps;
+      auto curr_node_edges = node_edges;
+
+      const auto neighbors = curr_node_edges[curr_node];
+      for (const auto neighbor : neighbors) {
+        curr_node_deps[neighbor].erase(curr_node);
+        curr_node_edges[curr_node].erase(neighbor);
+        if (curr_node_deps[neighbor].empty()) {
+          curr_start_nodes.push(neighbor);
+        }
+      }
+      self(self, curr_solution, curr_start_nodes, curr_node_deps,
+           curr_node_edges);
+    }
+    if (std::all_of(
+            node_deps.begin(), node_deps.end(),
+            [](const auto& nodeAndDeps) { return nodeAndDeps.second.empty(); }))
+      all_sorts.push_back(solution);
+  };
+  sort_all_recursive(sort_all_recursive, std::vector<int>{}, start_nodes,
+                     node_deps, node_edges);
+  return all_sorts;
+}
